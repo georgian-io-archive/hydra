7757029c5b8b3e6615dbedee30eeba9567c4300a
Tony Shen <tsa87@sfu.ca>
2020-10-05 10:06:12 -0700

    Install dependencies and execute a specified project

--- docker/trainer.sh
+++ docker/trainer.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-while getopts r:t: flag
+while getopts r:t:p: flag
 do
   case "${flag}" in
       r) git_url=${OPTARG};;
-      t) oauth_token=${OPTARG};
+      t) oauth_token=${OPTARG};;
+      p) project_name=${OPTARG};
   esac
 done
 
-echo "Github Repo URL: $git_url";
-git clone https://$oauth_token:x-oauth-basic@$git_url
+echo "Github Repo URL: $git_url"
+
+mkdir project
+cd project
+
+git clone https://$oauth_token:x-oauth-basic@$git_url .
+
+cd $project_name
+pip install -r requirements.txt
+python train.py
