f11b878ae9ea82d20052897a9400c88c74cfcbb8
Tony Shen <tonyshen@Tonys-MacBook-Pro.local>
2020-10-06 09:42:46 -0700

    Change project name locating to model path

--- docker/executor.sh
+++ docker/executor.sh
@@ -5,8 +5,7 @@ echo "Github Repo URL: $GIT_URL"
 mkdir project
 cd project
 
-git clone -b test-samples https://$OAUTH_TOKEN:x-oauth-basic@$GIT_URL .
+git clone https://$OAUTH_TOKEN:x-oauth-basic@$GIT_URL .
 
-cd $PROJECT_NAME
 pip3 install -r requirements.txt
-python3 train.py
+python3 $MODEL_PATH
