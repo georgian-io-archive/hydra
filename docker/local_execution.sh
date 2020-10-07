a8a385f899fd8501a58e3ae7e8a371401f0c0918
Tony Shen <tonyshen@Tonys-MacBook-Pro.local>
2020-10-07 09:43:46 -0700

    Log docker output into logs/docker folder in a time stamped log file

--- docker/local_execution.sh
+++ docker/local_execution.sh
@@ -1,8 +1,11 @@
 docker build -t hydra_image .
 
+# Create logs folder if it doesn't exist
+mkdir -p logs/docker
+
 docker run \
   -e GIT_URL=$1 \
   -e COMMIT_SHA=$2 \
   -e MODEL_PATH=$3 \
   -e OAUTH_TOKEN=$4 \
-  hydra_image:latest
+  hydra_image:latest > logs/docker/$(date +'%Y_%m_%d_%H_%M_%S').log
