463d4ceda1a7676e0e76f4c3a7f8a0ab5c4de59a
Tony Shen <tsa87@sfu.ca>
2020-10-08 09:58:58 -0700

    Fix docker build image issue (not building in hydra package folder

--- docker/executor.sh
+++ docker/executor.sh
@@ -1,5 +1,3 @@
-echo "Github Repo: https://$OAUTH_TOKEN:x-oauth-basic@${GIT_URL}/tree/${COMMIT_SHA}"
-
 mkdir project
 cd project
 
@@ -7,6 +5,5 @@ git clone https://$OAUTH_TOKEN:x-oauth-basic@$GIT_URL .
 git checkout $COMMIT_SHA
 
 conda env create -f environment.yml
-conda run -n hydra /bin/bash -c
 
 conda run -n hydra $PREFIX_PARAMS python3 $MODEL_PATH
--- docker/local_execution.sh
+++ docker/local_execution.sh
@@ -1,7 +1,11 @@
+
+DIR="$( dirname "${BASH_SOURCE[0]}" )"
+LOG_NAME=$(date +'%Y_%m_%d_%H_%M_%S')
+
+cd $DIR
 #TODO Bug fix building hydra image
 docker build -t hydra_image .
 # Create logs folder if it doesn't exist
-mkdir -p tmp/hydra
 
 docker run \
   -e GIT_URL=$1 \
@@ -9,4 +13,10 @@ docker run \
   -e OAUTH_TOKEN=$3 \
   -e MODEL_PATH=$4 \
   -e PREFIX_PARAMS=$5 \
-  hydra_image:latest > tmp/hydra/$(date +'%Y_%m_%d_%H_%M_%S').log
+  hydra_image:latest 2>&1 | tee ${LOG_NAME}.log
+
+cd -
+pwd
+mkdir -p tmp/hydra
+
+mv ${DIR}/${LOG_NAME}.log tmp/hydra/
