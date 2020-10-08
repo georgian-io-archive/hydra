a145c911abe6535123d9f58aef8f01d7dab740b2
Tony Shen <tonyshen@Tonys-MacBook-Pro.local>
2020-10-08 11:13:01 -0700

    Report exception for non-implemented parts

--- hydra/cli.py
+++ hydra/cli.py
@@ -11,20 +11,15 @@ from hydra.version import __version__
 def cli():
     pass
 
-
 @cli.command()
 @click.option('-m', '--model_path', required=True, type=str)
 @click.option('-c', '--cpu', default=16, type=click.IntRange(0, 128), help='Number of CPU cores required')
 @click.option('-r', '--memory', default=8, type=click.IntRange(0, 128), help='GB of RAM required')
-@click.option('--cloud', default='local', type=click.Choice(['fast_local','local', 'aws', 'gcp', 'azure'], case_sensitive=False))
+@click.option('--cloud', default='local', required=True, type=click.Choice(['fast_local','local', 'aws', 'gcp', 'azure'], case_sensitive=False))
 @click.option('--github_token', envvar='GITHUB_TOKEN') # Takes either an option or environment var
 @click.option('-b', '--branch', default='master', type=str)
 @click.option('-o', '--options', default='{}', type=str)
-
 def train(model_path, cpu, memory, github_token, cloud, branch, options):
-    click.echo("This is the training command")
-    click.echo("Running on {}".format(cloud))
-
     options = json.loads(options)
 
     prefix_params = ""
@@ -33,6 +28,7 @@ def train(model_path, cpu, memory, github_token, cloud, branch, options):
 
     if cloud == 'fast_local':
         subprocess.run([prefix_params, 'python3', model_path])
+        return 0
 
     check_repo(github_token, branch)
     git_url = subprocess.check_output("git config --get remote.origin.url", shell=True).decode("utf-8").strip()
@@ -44,3 +40,6 @@ def train(model_path, cpu, memory, github_token, cloud, branch, options):
         subprocess.run(
             ['sh', os.path.join(os.path.dirname(__file__), '../docker/local_execution.sh'),
              git_url, commit_sha, github_token, model_path, prefix_params])
+        return 0
+
+    raise Exception("Reached parts of Hydra that are not yet implemented.")
