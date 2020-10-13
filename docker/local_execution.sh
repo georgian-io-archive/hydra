4e3800f859d1aa031bf3bb61d31889f0276190ff
Tony Shen <tsa87@sfu.ca>
2020-10-13 12:51:57 -0700

    Implement the functionality of sumbitting jobs to GCP AI Platform in custom dockers

--- docker/local_execution.sh
+++ docker/local_execution.sh
@@ -1,21 +1,22 @@
-
+# Move to Hydra package's docker directory
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
-
-# Add random Hash
-LOG_NAME=$(date +'%Y_%m_%d_%H_%M_%S')
-
 cd $DIR
-docker build -t hydra_image .
 
-docker run \
-  -e GIT_URL=$1 \
-  -e COMMIT_SHA=$2 \
-  -e OAUTH_TOKEN=$3 \
-  -e MODEL_PATH=$4 \
-  -e PREFIX_PARAMS=$5 \
-  hydra_image:latest 2>&1 | tee ${LOG_NAME}.log
+# Generate identifier for this training job
+DATE=$(date +'%Y_%m_%d_%H_%M_%S')
+HASH=$(( RANDOM % 1000 ))
+JOB_NAME="job_${DATE}_id_${HASH}"
+
+# Build and run image
+docker build -t hydra_image .
+docker run hydra_image:latest \
+  --git_url=$1 \
+  --commit_sha=$2 \
+  --oauth_token=$3 \
+  --model_path=$4 \
+  --prefix_params=$5 \
+  2>&1 | tee ${JOB_NAME}.log
 
 # Move Log file to where the program is being called
-cd -
-mkdir -p tmp/hydra
-mv ${DIR}/${LOG_NAME}.log tmp/hydra/
+cd - && mkdir -p tmp/hydra
+mv ${DIR}/${JOB_NAME}.log tmp/hydra/
