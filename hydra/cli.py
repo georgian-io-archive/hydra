061374f5ad773f545424e739bc210b05b36102a7
Tony Shen <tsa87@sfu.ca>
2020-10-13 08:00:16 -0700

    Add help for option flag

--- hydra/cli.py
+++ hydra/cli.py
@@ -21,7 +21,7 @@ def hello(name):
 @click.option('-r', '--memory', default=8, type=click.IntRange(0, 128), help='GB of RAM required')
 @click.option('--cloud', default='local', required=True, type=click.Choice(['fast_local','local', 'aws', 'gcp', 'azure'], case_sensitive=False))
 @click.option('--github_token', envvar='GITHUB_TOKEN') # Takes either an option or environment var
-@click.option('-o', '--options', default='{}', type=str) #env #TODO Add help for options
+@click.option('-o', '--options', default='{}', type=str, help='Environmental variables for the script') 
 def train(model_path, cpu, memory, github_token, cloud, options):
     prefix_params = json_to_string(options)
 
