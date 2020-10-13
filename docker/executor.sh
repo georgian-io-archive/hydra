1c2fa283aa36d5cfcf3e89a75fefd214a337bc2b
Tony Shen <tonyshen@georgian.io>
2020-10-13 12:51:57 -0700

    Implement the functionality of sumbitting jobs to GCP AI Platform in custom dockers

--- docker/executor.sh
+++
@@ -1,9 +0,0 @@
-mkdir project
-cd project
-
-git clone https://$OAUTH_TOKEN:x-oauth-basic@$GIT_URL .
-git checkout $COMMIT_SHA
-
-conda env create -f environment.yml
-
-conda run -n hydra $PREFIX_PARAMS python3 $MODEL_PATH
--- hydra/cloud/google_cloud.py
+++ hydra/cloud/google_cloud.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from hydra.cloud.abstract_platform import AbstractPlatform
 
 class GoogleCloud(AbstractPlatform):
@@ -8,7 +10,12 @@ class GoogleCloud(AbstractPlatform):
         super().__init__(model_path, prefix_params)
 
     def train(self):
-        pass
+        execution_script_path = os.path.join(os.path.dirname(__file__), '../../docker/gcp_execution.sh')
+        command = ['sh', execution_script_path, self.git_url, self.commit_sha,
+            self.github_token, self.model_path, self.prefix_params]
+
+        subprocess.run(command)
+        return 0
 
     def serve(self):
         pass
