2f1493bcf32856807c04de9cb33d6da14f749923
Tony Shen <tonyshen@georgian.io>
2020-10-13 20:46:39 -0700

    Add option to sepcify tags, specify arguments using bash flags

--- hydra/cloud/google_cloud.py
+++ hydra/cloud/google_cloud.py
@@ -3,17 +3,20 @@ import subprocess
 from hydra.cloud.abstract_platform import AbstractPlatform
 
 class GoogleCloud(AbstractPlatform):
-    def __init__(self, model_path, prefix_params, git_url, commit_sha, github_token, region="us-west2"):
+    def __init__(self, model_path, prefix_params, git_url, commit_sha, github_token, tag, region="us-west2"):
         self.git_url = git_url
         self.commit_sha = commit_sha
         self.github_token = github_token
         self.region = region
+        self.tag = tag
         super().__init__(model_path, prefix_params)
 
     def train(self):
         execution_script_path = os.path.join(os.path.dirname(__file__), '../../docker/gcp_execution.sh')
-        command = ['sh', execution_script_path, self.git_url, self.commit_sha,
-            self.github_token, self.model_path, self.prefix_params, self.region]
+        command = ['sh', execution_script_path, '-g', self.git_url, '-c', self.commit_sha,
+            '-o', self.github_token, '-m', self.model_path, '-r', self.region, '-t', self.tag, '-p', self.prefix_params]
+
+        print(" ".join(command))
 
         subprocess.run(command)
         return 0
