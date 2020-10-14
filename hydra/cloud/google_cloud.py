08d8ce23fdbadfe645084f98f05d76f09a52d008
Tony Shen <tonyshen@georgian.io>
2020-10-13 19:15:58 -0700

    Allow user to set region of gcp compute

--- hydra/cloud/google_cloud.py
+++ hydra/cloud/google_cloud.py
@@ -3,16 +3,17 @@ import subprocess
 from hydra.cloud.abstract_platform import AbstractPlatform
 
 class GoogleCloud(AbstractPlatform):
-    def __init__(self, model_path, prefix_params, git_url, commit_sha, github_token):
+    def __init__(self, model_path, prefix_params, git_url, commit_sha, github_token, region="us-west2"):
         self.git_url = git_url
         self.commit_sha = commit_sha
         self.github_token = github_token
+        self.region = region
         super().__init__(model_path, prefix_params)
 
     def train(self):
         execution_script_path = os.path.join(os.path.dirname(__file__), '../../docker/gcp_execution.sh')
         command = ['sh', execution_script_path, self.git_url, self.commit_sha,
-            self.github_token, self.model_path, self.prefix_params]
+            self.github_token, self.model_path, self.prefix_params, self.region]
 
         subprocess.run(command)
         return 0
