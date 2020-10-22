5f1e82d773dfd5bab1380ec8a8e0f2699d4cd762
Tony Shen <tsa87@sfu.ca>
2020-10-22 09:29:48 -0700

    Skip download routine when file exist alreadyin local run

--- hydra/utils/storage.py
+++ hydra/utils/storage.py
@@ -1,10 +1,19 @@
+import os
 from google.cloud import storage
 
-def get_gcp_dataset(project_name, bucket_name, source_path, destination_path):
+def get_gcp_dataset(project_name, bucket_name, source_path, destination_filename):
     # project_name = "your-project-name"
     # bucket_name = "your-bucket-name"
     # source_path = "storage-object-name"
-    # destination_path = "local/path/to/file"
+    # destination_filename = "file-name"
+    destination_path = os.path.join('data', destination_filename)
+
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    if os.path.isfile(destination_path):
+        print("[PROJECT INFO] {} already exists, skipping download...".format(destination_path))
+        return
 
     client = storage.Client(project_name)
     bucket = client.get_bucket(bucket_name)
