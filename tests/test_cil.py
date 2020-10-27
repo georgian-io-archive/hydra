c94c00f434b70b1d148eabdda4a3960b83780116
Tony Shen <tsa87@sfu.ca>
2020-10-27 11:56:26 -0700

    Update test for yaml input options

--- tests/test_cil.py
+++ tests/test_cil.py
@@ -2,67 +2,66 @@ import pytest
 from hydra.cli import *
 from click.testing import CliRunner
 
-VALID_MODEL_PATH = "d3bug.py"
-VALID_REPO_URL = "https://georgian.io/"
-VALID_COMMIT_SHA = "m1rr0r1ng"
-VALID_FILE_PATH = "ones/and/zer0es"
-VALID_GITHUB_TOKEN =  "Georgian"
-VALID_OPTIONS = "{'epoch': 88}"
+TEST_YAML_PATH = "tests/resources/test.yaml"
+
+MODEL_PATH = "d3bug.py"
+REPO_URL = "https://georgian.io/"
+COMMIT_SHA = "m1rr0r1ng"
+FILE_PATH = "ones/and/zer0es"
+GITHUB_TOKEN =  "Georgian"
+JSON_OPTIONS = "{'epoch': 88}"
 
 CPU = 8
 MEMORY = 16
 GPU_TYPE = 'NVIDIA Tesla P4'
 GPU_COUNT = 1
-
 IMAGE_TAG = "default"
-IMAGE_URL = "snow/reggie.ie"
-
 REGION = "us-west2"
-
 OPTIONS = "epoch=88 lr=0.01"
-
 SCRIPT_PATH = "camp/flog/gnaw"
 MACHINE_NAME = "macbook-pro"
 
-def test_train_local(mocker):
-    def stub(dummy):
-        pass
-
+def test_train_local_cil(mocker):
+    mocker.patch(
+        "hydra.cli.os.path.isfile",
+        return_value=False
+    )
     mocker.patch(
         "hydra.cli.check_repo",
-        return_value=(VALID_REPO_URL, VALID_COMMIT_SHA)
+        return_value=(REPO_URL, COMMIT_SHA)
     )
     mocker.patch(
-        "hydra.cli.os.path.join",
-        return_value=VALID_FILE_PATH
+        "hydra.cli.json.loads",
     )
     mocker.patch(
-        "hydra.cli.json_to_string",
-        return_value=VALID_OPTIONS
+        'hydra.cli.inflate_options',
+        return_value=[JSON_OPTIONS]
     )
-
+    mocker.patch(
+        'hydra.cli.dict_to_string',
+        return_value=OPTIONS
+    )
     mocker.patch(
         'hydra.cli.LocalPlatform.__init__',
         return_value=None
     )
-
     mocker.patch(
         'hydra.cli.LocalPlatform.train',
     )
 
     runner = CliRunner()
     result = runner.invoke(train,
-        ['--model_path', VALID_MODEL_PATH,
+        ['--model_path', MODEL_PATH,
          '--cloud', 'local',
-         '--github_token', VALID_GITHUB_TOKEN,
-         '--options', VALID_OPTIONS])
+         '--github_token', GITHUB_TOKEN,
+         '--options', JSON_OPTIONS])
 
     LocalPlatform.__init__.assert_called_once_with(
-        model_path=VALID_MODEL_PATH,
-        options=VALID_OPTIONS,
-        git_url=VALID_REPO_URL,
-        commit_sha=VALID_COMMIT_SHA,
-        github_token=VALID_GITHUB_TOKEN,
+        model_path=MODEL_PATH,
+        options=OPTIONS,
+        git_url=REPO_URL,
+        commit_sha=COMMIT_SHA,
+        github_token=GITHUB_TOKEN,
     )
 
     LocalPlatform.train.assert_called_once_with()
@@ -71,58 +70,132 @@ def test_train_local(mocker):
 
 
 
-def test_train_google_cloud(mocker):
+def test_train_google_cloud_cli(mocker):
+    mocker.patch(
+        "hydra.cli.os.path.isfile",
+        return_value=False
+    )
     mocker.patch(
         "hydra.cli.check_repo",
-        return_value=(VALID_REPO_URL, VALID_COMMIT_SHA)
+        return_value=(REPO_URL, COMMIT_SHA)
     )
     mocker.patch(
-        "hydra.cli.os.path.join",
-        return_value=VALID_FILE_PATH
+        "hydra.cli.json.loads",
     )
     mocker.patch(
-        "hydra.cli.json_to_string",
-        return_value=VALID_OPTIONS
+        'hydra.cli.inflate_options',
+        return_value=[JSON_OPTIONS]
     )
-
+    mocker.patch(
+        'hydra.cli.dict_to_string',
+        return_value=OPTIONS
+    )
     mocker.patch(
         'hydra.cli.GoogleCloudPlatform.__init__',
         return_value=None
     )
-
     mocker.patch(
         'hydra.cli.GoogleCloudPlatform.train'
     )
 
     runner = CliRunner()
     result = runner.invoke(train,
-        ['--model_path', VALID_MODEL_PATH,
+        ['--model_path', MODEL_PATH,
          '--cloud', 'gcp',
-         '--github_token', VALID_GITHUB_TOKEN,
+         '--github_token', GITHUB_TOKEN,
          '--cpu_count', CPU,
          '--memory_size', MEMORY,
          '--gpu_count', GPU_COUNT,
          '--gpu_type', GPU_TYPE,
          '--region', REGION,
          '--image_tag', IMAGE_TAG,
-         '--image_url', IMAGE_URL,
-         '--options', VALID_OPTIONS])
+         '--options', JSON_OPTIONS])
 
     GoogleCloudPlatform.__init__.assert_called_once_with(
-        model_path=VALID_MODEL_PATH,
-        github_token=VALID_GITHUB_TOKEN,
+        model_path=MODEL_PATH,
+        github_token=GITHUB_TOKEN,
         cpu=CPU,
         memory=MEMORY,
         gpu_count=GPU_COUNT,
         gpu_type=GPU_TYPE,
         region=REGION,
-        git_url=VALID_REPO_URL,
-        commit_sha=VALID_COMMIT_SHA,
-        image_url=IMAGE_URL,
+        git_url=REPO_URL,
+        commit_sha=COMMIT_SHA,
         image_tag=IMAGE_TAG,
-        options=VALID_OPTIONS,
+        image_url='',
+        options=OPTIONS,
     )
 
     GoogleCloudPlatform.train.assert_called_once_with()
+
+    assert result.exit_code == 0
+
+
+def test_train_yaml(mocker):
+    mocker.patch(
+        "hydra.cli.os.path.isfile",
+        return_value=True
+    )
+    mocker.patch(
+        "hydra.cli.check_repo",
+        return_value=(REPO_URL, COMMIT_SHA)
+    )
+    mocker.patch(
+        'hydra.cli.LocalPlatform.__init__',
+        return_value=None
+    )
+    mocker.patch(
+        'hydra.cli.LocalPlatform.train',
+    )
+
+    runner = CliRunner()
+    result = runner.invoke(train,
+        ['-y', TEST_YAML_PATH,
+        '--github_token', GITHUB_TOKEN])
+
+    LocalPlatform.__init__.assert_has_calls(
+        [
+            mocker.call(
+                model_path=MODEL_PATH,
+                options='epoch=88 lr=0.01',
+                git_url=REPO_URL,
+                commit_sha=COMMIT_SHA,
+                github_token=GITHUB_TOKEN,
+            ),
+            mocker.call(
+                model_path=MODEL_PATH,
+                options='epoch=88 lr=0.1',
+                git_url=REPO_URL,
+                commit_sha=COMMIT_SHA,
+                github_token=GITHUB_TOKEN,
+            ),
+            mocker.call(
+                model_path=MODEL_PATH,
+                options='epoch=88 lr=1',
+                git_url=REPO_URL,
+                commit_sha=COMMIT_SHA,
+                github_token=GITHUB_TOKEN,
+            ),
+            mocker.call(
+                model_path=MODEL_PATH,
+                options='epoch=888 lr=0.1',
+                git_url=REPO_URL,
+                commit_sha=COMMIT_SHA,
+                github_token=GITHUB_TOKEN,
+            ),
+            mocker.call(
+                model_path=MODEL_PATH,
+                options='epoch=888 lr=1',
+                git_url=REPO_URL,
+                commit_sha=COMMIT_SHA,
+                github_token=GITHUB_TOKEN,
+            )
+        ],
+        any_order=True
+    )
+
+
+
+    LocalPlatform.train.assert_called_with()
 
     assert result.exit_code == 0
--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,8 +2,8 @@ import pytest
 import pytest_mock
 from hydra.utils.utils import *
 
-def test_json_to_string():
-    test_json = '{"depth":10, "epoch":100}'
-    result = json_to_string(test_json)
+def test_dict_to_string():
+    test_dict = {"depth":10, "epoch":100}
+    result = dict_to_string(test_dict)
 
     assert result == "depth=10 epoch=100"
