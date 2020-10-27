53c0101abf01988cea3c280980cea3d3535b3d4d
Tony Shen <tsa87@sfu.ca>
2020-10-26 17:24:34 -0700

    Update commit ID

--- docker/entry.py
+++ docker/entry.py
@@ -31,7 +31,7 @@ subprocess.run(["conda", "env", "create", "-f", "environment.yml"])
 
 # Temporary: Install hydra directly from github
 subprocess.run(["git", "clone", "https://{}:x-oauth-basic@{}".format(args.oauth_token, "github.com/georgianpartners/hydra"), "hydra"])
-subprocess.run(["git", "-C", "./hydra", "checkout", "32a4735"])
+subprocess.run(["git", "-C", "./hydra", "checkout", "4b9d0f3"])
 subprocess.run(["conda", "run", "-n", "hydra", "pip", "install", "-e", "hydra/"])
 
 for arg in args.options.split():
