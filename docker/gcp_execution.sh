ef31a54d40fc4f03d14bf679618b41844c9502b9
Tony Shen <tsa87@sfu.ca>
2020-10-28 14:32:51 -0700

    Remove streaming since it halts program's ablity to run more experiments

--- docker/gcp_execution.sh
+++ docker/gcp_execution.sh
@@ -94,7 +94,3 @@ gcloud ai-platform jobs describe $JOB_NAME 2>&1 | tee -a ${JOB_NAME}.log
 # Move Log file to where the program is being called
 cd ${PROJECT_DIR} && mkdir -p tmp/hydra
 mv ${DIR}/${JOB_NAME}.log tmp/hydra/${JOB_NAME}.log
-
-# Stream the logs
-echo "Streaming logs... Feel free to use CLTR+C to abort - It will not affect your submitted jobs."
-gcloud ai-platform jobs stream-logs $JOB_NAME 2>&1 | tee -a tmp/hydra/${JOB_NAME}.log
