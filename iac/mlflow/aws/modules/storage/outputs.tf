6db6d25e72ba111eb52320e3db9632e1a07ed7c7
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-01 03:25:06 -0500

    rename db in terraform

--- iac/mlflow/aws/modules/storage/outputs.tf
+++ iac/mlflow/aws/modules/storage/outputs.tf
@@ -1,9 +1,9 @@
 output "db_host" {
-  value       = aws_db_instance.mlflowdb.address
+  value       = aws_db_instance.mlflow_database.address
   description = "Hostname of RDS database instance"
 }
 
 output "db_name" {
-  value       = aws_db_instance.mlflowdb.name
+  value       = aws_db_instance.mlflow_database.name
   description = "Name of used RDS database"
 }
