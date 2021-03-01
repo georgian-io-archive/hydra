c4805a62ffd761994a577d8910264fd50ae1def9
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-01 04:58:37 -0500

    update docker_push scrip

--- iac/mlflow/aws/docker_push.sh
+++ iac/mlflow/aws/docker_push.sh
@@ -3,6 +3,6 @@ region="us-east-1"
 repository="mlflow-container-repository"
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${registry_id}.dkr.ecr.${region}.amazonaws.com"
-docker build -t hydra-mlflow-server-aws .
+docker build -t ${repository} .
 docker tag hydra-mlflow-server-aws:latest "${registry_id}.dkr.ecr.${region}.amazonaws.com/${repository}:latest"
 docker push "${registry_id}.dkr.ecr.${region}.amazonaws.com/${repository}:latest"
