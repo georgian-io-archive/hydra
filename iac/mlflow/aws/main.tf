1dad9125b0dca579331f1ff2de1600e0361b30ec
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-01 00:04:30 -0500

    modularize autoscaling script

--- iac/mlflow/aws/main.tf
+++ iac/mlflow/aws/main.tf
@@ -3,3 +3,17 @@
 provider "aws" {
   region = var.aws_region
 }
+
+module "autoscaling" {
+  source                        = "./modules/autoscaling"
+  ecs_service_name              = aws_ecs_service.service.name
+  max_tasks                     = 8
+  min_tasks                     = 2
+  server_cluster_name           = aws_ecs_cluster.mlflow_server_cluster.name
+  cpu_autoscale_in_cooldown     = 0
+  cpu_autoscale_out_cooldown    = 0
+  cpu_autoscale_target          = 80
+  memory_autoscale_in_cooldown  = 0
+  memory_autoscale_out_cooldown = 0
+  memory_autoscale_target       = 80
+}
