44f6552703ed1f6a78d0e289edb5902bd1e6a068
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-03 11:40:00 -0500

    add docker push script to repo with empty variable values

--- iac/aws/mlflow/docker_push.sh
+++ iac/aws/mlflow/docker_push.sh
@@ -1,6 +1,6 @@
 registry_id=$(aws ecr describe-registry | python3 -c "import sys, json; print(json.load(sys.stdin)['registryId'])")
-region="us-east-1"
-repository="mlflow-container-repository"
+region=""
+repository=""
 
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin "${registry_id}.dkr.ecr.${region}.amazonaws.com"
 docker build -t ${repository} .
