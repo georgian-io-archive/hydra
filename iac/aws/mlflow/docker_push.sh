3cf9d5d7b61f2f8c47039c3cdf9a94d91be86f10
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-03 11:04:22 -0500

    refactor directory structure

--- iac/aws/mlflow/docker_push.sh
+++ iac/aws/mlflow/docker_push.sh
@@ -2,7 +2,7 @@ registry_id=$(aws ecr describe-registry | python3 -c "import sys, json; print(js
 region="us-east-1"
 repository="mlflow-container-repository"
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${registry_id}.dkr.ecr.${region}.amazonaws.com"
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin "${registry_id}.dkr.ecr.${region}.amazonaws.com"
 docker build -t ${repository} .
 docker tag hydra-mlflow-server-aws:latest "${registry_id}.dkr.ecr.${region}.amazonaws.com/${repository}:latest"
 docker push "${registry_id}.dkr.ecr.${region}.amazonaws.com/${repository}:latest"
