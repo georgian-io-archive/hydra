c9854919fa1b60532db49bdf7cbc78e1161350e1
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-08 16:10:21 -0500

    update random credentials generated

--- iac/aws/mlflow/modules/secrets/main.tf
+++ iac/aws/mlflow/modules/secrets/main.tf
@@ -6,6 +6,7 @@ resource "random_string" "rds_username" {
 resource "random_password" "rds_password" {
   length  = var.password_length
   special = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
 resource "aws_secretsmanager_secret" "admin_username" {
@@ -15,7 +16,7 @@ resource "aws_secretsmanager_secret" "admin_username" {
 
 resource "aws_secretsmanager_secret_version" "secret_username" {
   secret_id     = aws_secretsmanager_secret.admin_username.id
-  secret_string = random_string.rds_username.result
+  secret_string = "mlflow${random_string.rds_username.result}"
 }
 
 resource "aws_secretsmanager_secret" "admin_password" {
