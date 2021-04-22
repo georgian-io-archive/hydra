fe4127c1343d068595ce71baf90ee5e8928c1c86
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-04-22 19:49:59 -0400

    add permissions module to batch IaC

---
+++ iac/aws/batch/modules/permissions/main.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "compute_environment_service_role" {
+  name                = var.compute_environment_service_role_name
+  assume_role_policy  = data.aws_iam_policy_document.compute_environment_service_role_policy.json
+}
+
+data "aws_iam_policy_document" "compute_environment_service_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["batch.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "compute_environment_service_role_policy_attachment" {
+  role        = aws_iam_role.compute_environment_service_role.name
+  count       = length(var.compute_environment_service_iam_policy_arn)
+  policy_arn  = var.compute_environment_service_iam_policy_arn[count.index]
+}
+
+resource "aws_iam_instance_profile" "compute_environment_instance_profile" {
+  name = var.compute_environment_instance_role_name
+  role = aws_iam_role.compute_environment_instance_role.name
+}
+
+resource "aws_iam_role" "compute_environment_instance_role" {
+  name                = var.compute_environment_instance_role_name
+  assume_role_policy  = data.aws_iam_policy_document.compute_environment_instance_role_policy.json
+}
+
+data "aws_iam_policy_document" "compute_environment_instance_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "compute_environment_instance_role_policy_attachment" {
+  role        = aws_iam_role.compute_environment_instance_role.name
+  count       = length(var.compute_environment_instance_iam_policy_arn)
+  policy_arn  = var.compute_environment_instance_iam_policy_arn[count.index]
+}
