fdb12fc41c028d1a27a8a796456a1ae6cef1e41b
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-04-22 19:48:18 -0400

    add lambda component to batch IaC

---
+++ iac/aws/batch/modules/lambda/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "lambda_service_role" {
+  name                = var.lambda_service_role_name
+  assume_role_policy  = data.aws_iam_policy_document.lambda_service_role_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_service_role_policy" {
+  version   = "2012-10-17"
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_service_role_policy_attachment" {
+  role        = aws_iam_role.lambda_service_role.name
+  count       = length(var.lambda_service_iam_policy_arn)
+  policy_arn  = var.lambda_service_iam_policy_arn[count.index]
+}
+
+resource "aws_lambda_function" "initialize_db" {
+  filename      = var.lambda_function_file_path
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.lambda_service_role.arn
+  timeout       = var.lambda_function_timeout
+  handler       = "test.initialize_db"
+  runtime       = "python3.8"
+
+  vpc_config {
+    security_group_ids  = var.lambda_security_group_ids
+    subnet_ids          = var.lambda_subnets
+  }
+}
+
+data "aws_lambda_invocation" "db_init" {
+  function_name = aws_lambda_function.initialize_db.function_name
+
+  input = <<JSON
+  {
+    "table_setup_script_bucket_name" : "${var.table_setup_script_bucket_name}",
+    "table_setup_script_bucket_key" : "${var.table_setup_script_bucket_key}",
+    "database_hostname" : "${var.database_hostname}",
+    "database_username_secret" : "${var.database_username_secret}",
+    "database_password_secret" : "${var.database_password_secret}",
+    "database_default_name" : "${var.database_default_name}",
+    "aws_region" : "${var.aws_region}"
+  }
+  JSON
+}
---
+++ iac/aws/batch/modules/lambda/variables.tf
@@ -0,0 +1,69 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "lambda_service_role_name" {
+  description = "IAM name of the lambda function service role"
+  type        = string
+}
+
+variable "lambda_function_file_path" {
+  description = "File path of the lambda ZIP file that will be executed"
+  type        = string
+}
+
+variable "lambda_function_timeout" {
+  description = "Timeout of the executed lambda function"
+  type        = number
+}
+
+variable "lambda_service_iam_policy_arn" {
+  description = "IAM policies attached to the lambda function service role"
+  type        = list(string)
+}
+
+variable "lambda_function_name" {
+  description = "Name of the lambda function that will be created"
+  type        = string
+}
+
+variable "lambda_security_group_ids" {
+  description = "List of security groups to attach lambda function to"
+  type        = list(string)
+}
+
+variable "lambda_subnets" {
+  description = "List of subnets to attach lambda function to"
+  type        = list(string)
+}
+
+variable "database_hostname" {
+  description = "Hostname of the Batch RDS instance"
+  type        = string
+}
+
+variable "database_username_secret" {
+  description = "Secret name of the Batch RDS Username"
+  type        = string
+}
+
+variable "database_password_secret" {
+  description = "Secret name of the Batch RDS Password"
+  type        = string
+}
+
+variable "database_default_name" {
+  description = "Default database name created in RDS instance"
+  type        = string
+}
+
+variable "table_setup_script_bucket_name" {
+  description = "The name of the S3 bucket that will store the table setup script"
+  type        = string
+}
+
+variable "table_setup_script_bucket_key" {
+  description = "The key of the S3 bucket that will store the table setup script"
+  type        = string
+}
